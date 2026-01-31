a60848e8ad8fc9a9d3b4819efc126debacfa7298
MrPowerpoint <tiziand4404@gmail.com>
2026-01-31 15:39:35 +0100

    [*] id und so kram

--- Assets/02_Code/PlayerMaskManager.cs
+++ Assets/02_Code/PlayerMaskManager.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 public class PlayerMaskManager
 {
     public static float switchCooldown = 1f;
-    public float CurrentMaskChangeCooldown => Mathf.Clamp(Time.time - lastMaskChange,0,switchCooldown)/switchCooldown;
+    public float CurrentMaskChangeCooldown => Mathf.Clamp(Time.time - lastMaskChange, 0, switchCooldown) / switchCooldown;
     private float lastMaskChange = float.MinValue;
     List<Mask> masks = new List<Mask>();
     public delegate void OnMaskChange(int id);
     public event OnMaskChange maskChange;
     public int currentID = 0;
     public Mask CurrentMask => masks[currentID];
-    public void Init(IEnumerable<int> maskIDs) 
+    public void Init(IEnumerable<int> maskIDs)
     {
-        foreach (int maskID in maskIDs) 
+        foreach (int maskID in maskIDs)
         {
-            if (!AllMasks.Masks.ContainsKey(maskID)) 
+            if (!AllMasks.Masks.ContainsKey(maskID))
             {
                 continue;
             }
             masks.Add(AllMasks.Masks[maskID]);
         }
     }
-    public Mask ChangeMask(int maskID) 
+    public Mask ChangeMask(int maskID)
     {
-        if(Time.time - lastMaskChange< switchCooldown) 
+        if (Time.time - lastMaskChange < switchCooldown ||maskID ==currentID)
         {
             return CurrentMask;
         }
         lastMaskChange = Time.time;
-        int i = 0;
-        foreach (Mask mask in masks) 
-        {
-            if(mask.GetID() == maskID) 
-            {
-                CurrentMask.Deactivate();
-                currentID = i;
-                maskChange?.Invoke(currentID);
-                CurrentMask.Activate();
-            }
-            i++;
-        }
+        CurrentMask.Deactivate();
+        currentID = maskID;
+        maskChange?.Invoke(currentID);
+        CurrentMask.Activate();
+
         return CurrentMask;
     }
 }
